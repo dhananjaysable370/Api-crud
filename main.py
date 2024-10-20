f5a6836f3a598ef05a26cdda9148f9e55b5437f5
Dhananjay <dhananjaysable370@gmail.com>
2024-10-20 14:44:48 +0530

    Api Practice

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 import random
 
-ai = random.randint(1, 100)
-user = -1
-attempts = 1
-minAttempts = 10
 
-while minAttempts > 0 and ai != user:
-    if minAttempts == 0:
-        print("Game over!")
-        break
-    print(f"Your remaining attempts {minAttempts}")
-    user = int(input("Enter a number between 1 to 100 : "))
-    if user > ai:
-        print("Your guess is too high!")
-    elif user < ai:
-        print("Your guess is too low!")
-    else:
-        if attempts == 1:
-            print(f"You guessed it in {attempts} Attempt.")
+def guessGame():
+    ai = random.randint(1, 100)
+    user = -1
+    attempts = 1
+    minAttempts = 10
+
+    while ai != user:
+        if minAttempts == 0:
+            print(f"Game over! random number was {ai}")
+            break
+        print(f"Your remaining attempts {minAttempts}")
+        print()
+        user = int(input("Enter a number between 1 to 100 : "))
+        if user > ai:
+            print("Your guess is too high!")
+            print()
+        elif user < ai:
+            print("Your guess is too low!")
+            print()
         else:
-            print(f"You guessed it in {attempts} Attempts..")
-    attempts += 1
-    minAttempts -= 1
+            if attempts == 1:
+                print(f"You guessed it in {attempts} Attempt.")
+                print()
+            elif user == ai:
+                print(f"You guessed it in {attempts} Attempts..")
+                print()
 
-# def passwordGenerator(length):
-#     source = string.ascii_letters + string.digits
-#     password = ""
-#     for i in range(length):
-#         password = random.choice(source)
-#         print(password, end="")
+        attempts += 1
+        minAttempts -= 1
 
-
-# passwordGenerator(8)
-
-
-# def guessGame(number):
+guessGame()
